0a937bf0ec0957786f7d742412181e4f71e68da1
Vinicius Zorzanelli <vinizorza@hotmail.com>
2017-09-19 08:18:24 -0300

    Update Cliente.php

--- Source/Modelo/Cliente.php
+++ Source/Modelo/Cliente.php
@@ -1,50 +1,49 @@
 <?php
 
 abstract class Cliente {
-
-    protected var $nome;
+	
+	protected var $nome;
 	protected var $telefone;
 	protected var $email;
 	protected var $id;
 	
-
-    function Cliente($nome, $telefone, $email){
-        $this->nome = $nome;
-        $this->telefone = $telefone;
+	function Cliente($nome, $telefone, $email){
+        	$this->nome = $nome;
+        	$this->telefone = $telefone;
 		$this->email = $email;
-    }
+    	}
 
-    function getNome(){
-        return $this->nome;
-    }
+	function getNome(){
+        	return $this->nome;
+    	}
 	
 	function setNome($value){
-        $this->nome = $value;
-    }
+        	$this->nome = $value;
+    	}
 
-    function getTelefone(){
-        return $this->telefone;
-    }
+	function getTelefone(){
+        	return $this->telefone;
+    	}
 	
 	function setTelefone($value){
-        $this->telefone = $value;
-    }
+        	$this->telefone = $value;
+    	}
 	
 	function getEmail(){
-        return $this->email;
-    }
+        	return $this->email;
+    	}
 	
 	function setEmail($value){
-        $this->email = $value;
-    }
+        	$this->email = $value;
+    	}
 	
 	function getId(){
-        return $this->id;
-    }
+        	return $this->id;
+    	}
 	
 	function setId($value){
-        $this->id = $value;
-    }
+        	$this->id = $value;
+    	}
 
 
 }
