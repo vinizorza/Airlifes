a5b514cae39d929a6090a9bf491cb9775b32c480
Vinicius Zorzanelli <vinizorza@hotmail.com>
2017-09-19 08:22:26 -0300

    Update PessoaJuridica.php

--- Source/Modelo/PessoaJuridica.php
+++ Source/Modelo/PessoaJuridica.php
@@ -4,18 +4,18 @@ include_once 'Cliente.php';
 
 class PessoaJuridica extends Cliente{
 
-    private var $cnpj;	
+	private var $cnpj;	
 
-    function Cliente($nome, $telefone, $email, $cnpj){
-        parent::Cliente($nome, $telefone, $email);
+	function Cliente($nome, $telefone, $email, $cnpj){
+        	parent::Cliente($nome, $telefone, $email);
 		$this->$cnpj = $cnpj;
-    }
+    	}
 	
 	function getCnpj(){
-        return $this->cnpj;
-    }
+        	return $this->cnpj;
+    	}
 	
 	function setCnpj($value){
-        $this->cnpj = $value;
-    }
+        	$this->cnpj = $value;
+    	}
 }
