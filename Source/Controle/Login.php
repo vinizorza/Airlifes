b9018ace906884c6bc1eace971909efa0eaded7f
Vinicius Zorzanelli <vinizorza@hotmail.com>
2017-09-25 21:23:21 -0300

    ieie

--- Source/Controle/Login.php
+++ Source/Controle/Login.php
@@ -1,7 +1,12 @@
 <?php
 
+require_once('../Modelo/Cliente.php');
+
 //Recebendo os valores
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
-echo $email;
+$cliente = new Cliente("a", "a", "a", "a");
+echo $cliente->getCpf();
+
+//echo $email;
